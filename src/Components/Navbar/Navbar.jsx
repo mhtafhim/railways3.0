17e9723c01b58fe675fcf82cf6c56c9b53e9f0cd
ShadmanChowdhury007 <shadman.cse.20200204019@aust.edu>
2024-01-28 00:26:48 +0600

    Shaddy Testing ui

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,10 +12,10 @@ export const Navbar = () => {
         <p>Railway E-Service</p>
      </div>
      <ul className="nav-menu">
-        <li onClick={()=>{setMenu("Railway")}}><Link style={{textDecoration:'none', color: "white" }} to='/railway'>Railway</Link>{menu==="railway"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("TrainSchedule")}}><Link style={{textDecoration:'none', color: "white" }} to='/Train Schedule'>Train Schedule</Link>{menu==="Train Schedule"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("Live Location")}}><Link style={{textDecoration:'none', color: "white" }} to='/Live Location'>Live Location</Link>{menu==="Live Location"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("Book")}}><Link style={{textDecoration:'none', color: "white" }} to='/Booking'>Booking</Link>{menu==="Book"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("Railway")}}><Link style={{textDecoration:'none', color: "black" }} to='/railway'>Railway</Link>{menu==="railway"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("TrainSchedule")}}><Link style={{textDecoration:'none', color: "black" }} to='/Train Schedule'>Train Schedule</Link>{menu==="Train Schedule"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("Live Location")}}><Link style={{textDecoration:'none', color: "black" }} to='/Live Location'>Live Location</Link>{menu==="Live Location"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("Book")}}><Link style={{textDecoration:'none', color: "black" }} to='/Booking'>Booking</Link>{menu==="Book"?<hr/>:<></>}</li>
         
      </ul>
      <div className="nav-login">
